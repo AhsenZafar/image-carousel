45a37152c7d8c5ba08a2b061ee5e53983ecaa8f0
AhsenZafar <100182384+AhsenZafar@users.noreply.github.com>
2022-09-02 19:38:17 -0600

    finished js code to scroll carousel

--- code.js
+++ code.js
@@ -1,26 +1,37 @@
-const items = document.getElementsByClassName("carousel-item")
-let slidePosition = 0
-items[slidePosition].style.display ="visible"
-console.log(items[slidePosition])
-const totaSlides = items.length
-document.getElementById("carousel-button-next").addEventListener("click",function(){
-   if (slidePosition <2){
-        slidePosition += 1
-    }else{slidePosition =0}
-    for (let i =0; i<items.length;i++ ){
-        if(items[i])=== items[slidePosition]){
-            items[i].style.display = "visible"
-        }else{items[i].style.display = "none"}
+const slides = document.getElementsByClassName('carousel-item');
+let slidePosition = 0;
+const totalSlides = slides.length;
+
+document.getElementById('carousel-button-next').addEventListener('click', moveToNextSlide);
+document.getElementById('carousel-button-prev').addEventListener('click', moveToPrevSlide);
+
+function hideAllSlides() {
+    for (let slide of slides) {
+        slide.classList.remove('carousel-item-visible');
+        slide.classList.add('carousel-item-hidden');
     }
-})
-document.getElementById("carousel-button-prev").addEventListener("click",function(){
-    if (slidePosition > 0){slidePosition -= 1}
-    else{slidePosition = 2}
-    for (let i =0; i<items.length;i==){
-        if(items[i]=== items[slidePosition]){
-            items[i].style.display = "visible"
-        }else{
-            items[i].style.display = "none"
-        }
+}
+
+function moveToNextSlide() {
+    hideAllSlides();
+    
+    if (slidePosition === totalSlides - 1) {
+        slidePosition = 0;
+    } else {
+        slidePosition++;
     }
-})
+    
+    slides[slidePosition].classList.add("carousel-item-visible");
+}
+
+function moveToPrevSlide() {
+    hideAllSlides();
+    
+    if (slidePosition === 0) {
+        slidePosition = totalSlides - 1;
+    } else {
+        slidePosition--;
+    }
+    
+    slides[slidePosition].classList.add("carousel-item-visible");
+}
